359d0314f4c4f46637fd9722d349d80312a33079
AizawaAkitoshi <akitoshi.aizawa_211000@glv.co.jp>
2023-06-27 10:10:36 +0900

    ユーザー情報表示の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,15 +4,16 @@ class UsersController < ApplicationController
   end
 
   def update
-    current_user.update(user_params)
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
 
   def user_params
     params.require(:user).parmit(:name,:email)
-    redirect_to root_path
-  else
-    render :edit, status: :unprocessable_entity
   end
 end
